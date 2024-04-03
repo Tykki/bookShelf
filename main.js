f00c1e34d175801ded7e55aa21fe7ebd74077b0b
Tykki Etchie <ayiri.etchie@gmail.com>
2024-04-03 11:59:10 -0500

    got to make my book flip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import './style.css'
+import './assets/css/style.css'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import './assets/js/book.js'
 
 // https://codepen.io/jkantner/pen/xrRPRL
 // https://codepen.io/lina994/pen/qBXGvby
