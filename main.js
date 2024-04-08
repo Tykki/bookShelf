ce7c1c8f60bbec9b3ba37d89172f0cb55b784b96
Tykki Etchie <ayiri.etchie@gmail.com>
2024-04-08 12:01:21 -0500

    added nav logic

--- main.js
+++ main.js
@@ -3,5 +3,43 @@ import './assets/js/book.js'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-// https://codepen.io/jkantner/pen/xrRPRL
-// https://codepen.io/lina994/pen/qBXGvby
+const pages = document.querySelectorAll('main')
+const links = document.querySelectorAll('.nav-link')
+
+let flag;
+const hide = () => {
+    pages.forEach(page =>{
+        page.classList.add('d-none')
+    })
+    links.forEach(link =>{
+        link.classList.remove('active')
+    })
+}
+const hashHandler = (hash) => {
+    console.log('step 2', hash)
+    if (!hash) {
+        return
+    }
+    hide()
+    flag = false
+    // Active Class on navbar if we doing that
+    pages.forEach((page) => {
+        if (hash === `#${page.id}`) {
+            console.log('pass')
+            page.classList.remove('d-none')
+            flag = true
+        }
+    })
+    // check if a page was set, if not show home page
+    if (!flag){
+        location.hash = '#home'
+    }
+    links.forEach(link => {
+        // console.log(link.getAttribute('href'), location.hash)
+        if (location.hash === link.getAttribute('href')){
+            link.classList.add('active')
+        }
+    })
+ }
+window.addEventListener('hashchange', (e) => hashHandler(e.target.location.hash))
+hashHandler(window.location.hash)
