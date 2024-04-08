0d9ac3f647e7a57627261c477d7fccad466dea4f
Tykki <14112429+Tykki@users.noreply.github.com>
2024-04-08 08:24:42 -0500

    book api picked, just need to finish layout and add content

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,25 +1,54 @@
-let currentPage = 1;
+import {PageFlip} from 'page-flip';
 
-function toggleClass(e, toggleClassName) {
-  if(e.className.includes(toggleClassName)) {
-    e.className = e.className.replace(' ' + toggleClassName, '');
-  } else {
-    e.className += ' ' + toggleClassName;
-  }
-}
+document.addEventListener('DOMContentLoaded', function() {
 
-const movePage = (e, page) => {
-  if (page == currentPage) {
-    currentPage+=2;
-    toggleClass(e, "left-side");
-    toggleClass(e.nextElementSibling, "left-side");
-    
-  }
-  else if (page = currentPage - 1) {
-    currentPage-=2;
-    toggleClass(e, "left-side");
-    toggleClass(e.previousElementSibling, "left-side");
-  }
-  
-}
-console.log(movePage)
+    const pageFlip = new PageFlip(
+        document.getElementById("demoBookExample"),
+        {
+            width: 550, // base page width
+            height: 733, // base page height
+
+            size: "stretch",
+            // set threshold values:
+            minWidth: 315,
+            maxWidth: 1000,
+            minHeight: 420,
+            maxHeight: 1350,
+
+            maxShadowOpacity: 0.5, // Half shadow intensity
+            showCover: true,
+            mobileScrollSupport: false // disable content scrolling on mobile devices
+        }
+    );
+
+    // load pages
+    pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+    document.querySelector(".page-total").innerText = pageFlip.getPageCount();
+    document.querySelector(
+        ".page-orientation"
+    ).innerText = pageFlip.getOrientation();
+
+    document.querySelector(".btn-prev").addEventListener("click", () => {
+        pageFlip.flipPrev(); // Turn to the previous page (with animation)
+    });
+
+    document.querySelector(".btn-next").addEventListener("click", () => {
+        pageFlip.flipNext(); // Turn to the next page (with animation)
+    });
+
+    // triggered by page turning
+    pageFlip.on("flip", (e) => {
+        document.querySelector(".page-current").innerText = e.data + 1;
+    });
+
+    // triggered when the state of the book changes
+    pageFlip.on("changeState", (e) => {
+        document.querySelector(".page-state").innerText = e.data;
+    });
+
+    // triggered when page orientation changes
+    pageFlip.on("changeOrientation", (e) => {
+        document.querySelector(".page-orientation").innerText = e.data;
+    });
+});
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './assets/css/style.css'
+import './assets/js/book.js'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-import './assets/js/book.js'
 
 // https://codepen.io/jkantner/pen/xrRPRL
 // https://codepen.io/lina994/pen/qBXGvby
